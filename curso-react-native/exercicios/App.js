317569e7144c0f9521b9b80c83647ea14d1e0041
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-04-21 00:10:47 -0300

    React

--- curso-react-native/exercicios/App.js
+++ curso-react-native/exercicios/App.js
@@ -1,35 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react'
 import {View, StyleSheet } from 'react-native'
-import Aleatorio from './src/components/Aleatorio';
-import MinMax from './src/components/MinMax'
-import X, { Comp1,Comp2 } from './src/components/Multi'
-import Primeiro from './src/components/Primeiro'
+import Botao  from './src/components/Botao';
+//import Aleatorio from './src/components/Aleatorio';
+//import MinMax from './src/components/MinMax'
+//import X, { Comp1,Comp2 } from './src/components/Multi'
+//import Primeiro from './src/components/Primeiro'
 
-import Titulo from './components/Titulo'
+//import Titulo from './src/components/Titulo'
 
 
 export default () => {
     <View style={style.App}>
-        <View style={style.App}>
-            <Titulo principal="Cadastro"
-                segundario="Tela de Cadastro do Produto"/>
-                {}
-        </View>
-        <Aleatorio min={1} max={60} />
-        <Aleatorio min={1} max={60} />
-        <Aleatorio min={1} max={60} />
-        <Aleatorio min={1} max={60} />
-        <Aleatorio min={1} max={60} />
-        <Aleatorio min={1} max={60} />
-        <MinMax min={3} max={20} />
-        <MinMax min={1} max={93} />
-        <X />
-        <Comp1 />
-        <Comp2 />
-        <Primeiro />
-        <StatusBar style="auto" />
+        <Botao />
     </View>
+    // <View style={style.App}>
+    //     <View style={style.App}>
+    //         <Titulo principal="Cadastro"
+    //             segundario="Tela de Cadastro do Produto"/>
+    //             {}
+    //     </View>
+    //     <Aleatorio min={1} max={60} />
+    //     <Aleatorio min={1} max={60} />
+    //     <Aleatorio min={1} max={60} />
+    //     <Aleatorio min={1} max={60} />
+    //     <Aleatorio min={1} max={60} />
+    //     <Aleatorio min={1} max={60} />
+    //     <MinMax min={3} max={20} />
+    //     <MinMax min={1} max={93} />
+    //     <X />
+    //     <Comp1 />
+    //     <Comp2 />
+    //     <Primeiro />
+    //     <StatusBar style="auto" />
+    // </View>
 }
 
 const style = StyleSheet.create({
