549a42954060be1398958b1d056c393d913d91e9
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-04-26 00:20:06 -0300

    react

--- curso-react-native/calculator/App.js
+++ curso-react-native/calculator/App.js
@@ -2,14 +2,29 @@ import { StatusBar } from 'expo-status-bar';
 import React, {Component} from 'react';
 import { render } from 'react-dom';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from './components/Button';
 
-
-export default  App extends Component {
+export default class App extends Component {
   render() {
   return (
     <View style={styles.container}>
-      <Texf ></Text>
-      <StatusBar style="auto" />
+      <View style={styles.buttons}>
+        <Button label='AC' />
+        <Button label='/' />
+        <Button label='7' />
+        <Button label='8' />
+        <Button label='9' />
+        <Button label='*' />
+        <Button label='4' />
+        <Button label='5' />
+        <Button label='6' />
+        <Button label='-' />
+        <Button label='1' />
+        <Button label='2' />
+        <Button label='3' />
+        <Button label='+' />
+        <Button label='.' />
+      </View>
     </View>
   );
   }
@@ -18,8 +33,12 @@ export default  App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  
   },
+  buttons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+ 
+  },
+
 });
