caf0e2262d03a1f24028acba6ad9b0e26633eb1e
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-04-30 10:56:43 -0300

    React

--- curso-react-native/mines/App.js
+++ curso-react-native/mines/App.js
@@ -4,8 +4,28 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Component } from 'react/cjs/react.production.min';
 import Field from './src/components/Field';
 import params  from './src/params'
+import MineField from './src/components/MineField';
+
+import {
+  createMinedBoard
+} from './src/functions'
 
 export default class App extends Component {
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = paras.getRowsAmount()
+    return {
+      
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
