1a21701453687ee4090fa934460b032961eeccf1
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-05-10 00:08:03 -0300

    react

--- curso-react-native/tasks/src/components/Task.js
+++ curso-react-native/tasks/src/components/Task.js
@@ -4,12 +4,30 @@ import { View, Text, } from 'react-native'
 
 export default props => {
     return (
+        <View style={styles.container}>
+            <View style={styles.checkContainer}>
+
+            </View>
+
         <View>
             <Text>{props.desc}</Text>
             <Text>{props.estimateAt + ""}</Text>
-            <Text>{props.doneAt + ""}</Text>
+        </View>
         </View>
     )
+}
+
+function getCheckView(doneAt) {
+    if(doneAt != null) {
+        return (
+            <View><Text>Concluida</Text></View>
+        )
+    
+    } else {
+        return (
+            <View><Text>Pendente</Text></View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
@@ -17,6 +35,11 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         borderColor: '#AAA',
         boderBottomWidth: 1,
+        alignItems: 'center',
+        paddingVertical: 10
 
+    },
+    checkContainer: {
+        width: '20%'
     }
 })
