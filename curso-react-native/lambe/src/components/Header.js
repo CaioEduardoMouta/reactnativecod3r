57611be5d133216bed6ed1a20ea79f358811f2d6
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-05-22 23:37:17 -0300

    react

--- curso-react-native/lambe/src/components/Header.js
+++ curso-react-native/lambe/src/components/Header.js
@@ -14,9 +14,39 @@ class Header extends Component {
         return (
             <View style={styles.container}>
                 <View style={styles.rowContainer}>
-                    
+                    <Image source={icon} style={styles.image} />
+                    <Text style={style.title}>Lambe Lambe</Text>
                 </View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: Platform.OS === 'ios' ? 20 : 0,
+        padding: 10,
+        borderBottomWidth: 1,
+        borderColor: '#BBB'
+    },
+
+    rowContainer: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    image: {
+        height: 30,
+        width: 30,
+        resizeMode: 'contain'
+    },
+    title: {
+        color: '#000',
+        fontFamily: 'shelter',
+        height: 30,
+        fontSize: 28
+
+    }
+
+})
+
+export default Header 
