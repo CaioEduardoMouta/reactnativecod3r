1e2bb26418a646204d6aebf6b7c06c36df6cff1f
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-05-25 00:04:28 -0300

    react

---
+++ curso-react-native/lambe/src/screens/Feed.js
@@ -0,0 +1,40 @@
+import React, { Componen, Component } from 'react'
+import { StyleSheet, FlatList, View } from 'react-native'
+import Header from '../components/Header'
+import Post  from '../components/Post'
+
+class Feed extends Component {
+    state = { 
+        posts: [{
+            id: Math.random(),
+            nickmame: 'Caio Mouta',
+            email: 'caiomouta@gmail.com',
+            image: require('../../assets/imgs/fence.jpg'),
+            comments: [{
+                nickname: 'Carrazedo',
+                comment: 'Stunning'
+            }, {
+                nickmame: 'Monteiro',
+                comment: 'Orra'
+            }]
+        }, {
+            id: Math.random(),
+            nickmame: 'Francisco Leandro Lima',
+            email: 'fllima@gmail.com',
+            Image: require('../../assets/imgs/bw.jpg'),
+            comments:[]
+        }]
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Header />
+                <FlatList
+                    data={this.state.posts}
+                    keyExtractor={item => `${item.id}`}
+                    renderItem={({ item}) => }
+            </View>
+        )
+    }
+}
