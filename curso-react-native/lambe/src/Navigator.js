e4201e49859091573565270ce26909b6c6c11ce3
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-05-28 20:32:53 -0300

    react

--- curso-react-native/lambe/src/Navigator.js
+++ curso-react-native/lambe/src/Navigator.js
@@ -1,10 +1,30 @@
 import React  from 'react'
-import { createButtonTabNavigator } from 'react-navigation'
+import { 
+    createButtonTabNavigator,
+    createSwitchNavigator,
+    createStackNavigator
+    } from 'react-navigation'
 import { Icon } from 'react-native-vector-icons/FontAwesome'
 
 import Feed from './screens/Feed'
 import AddPhoto from './screens/AddPhoto'
 import Profile from './screens/Profile'
+import Login  from './screens/Login'
+import Register from './screens/Register'
+
+const authRouter = createStackNavigator({
+    Login: { screen: Login, navigationOptions: { title:'Login' }},
+    Register: { screen: Register, NavigatorOptions: { title: 'Register'} }
+}, {
+    initialRouteName: 'Login'
+})
+
+const loginOrProfileRouter = createSwitchNavigator ({
+    Profile: Profile,
+    Auth: authRouter
+}, {
+    initialRouteName: 'Profile'
+})
 
 const MenuRoutes = {
     Feed: {
@@ -28,7 +48,7 @@ const MenuRoutes = {
     },
     Profile: {
         name: 'Profile',
-        screen: Profile,
+        screen: loginOrProfileRouter,
         navigationOptions: {
             title: 'Profile',
             tabBarIcon: ({ tintColor: color }) =>
